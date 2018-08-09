b06f76a91fa4c14ca1f8c2a7b963d73359e147bd
TCIG-Arvind <arvind@mailcohen.com>
2018-08-09 19:13:49 +0300

    Add project files.

---
+++ TravelAssistant/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Services;
+using TravelAssistant;
+
+namespace Discovery.ListAPIs
+{
+    /// <summary>
+    /// This example uses the discovery API to list all APIs in the discovery repository.
+    /// https://developers.google.com/discovery/v1/using.
+    /// <summary>
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Travesys Bot Assistant");
+            Console.WriteLine("====================");
+
+            new TravesysAI();
+
+            //try
+            //{
+            //    new Program().Run().Wait();
+            //}
+            //catch (AggregateException ex)
+            //{
+            //    foreach (var e in ex.InnerExceptions)
+            //    {
+            //        Console.WriteLine("ERROR: " + e.Message);
+            //    }
+            //}
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            // Create the service.
+            var service = new DiscoveryService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Travesys Travel Assistant",
+                ApiKey = "AIzaSyCKyGF5N5SU1XrKeK-RlB16mANksrCCrhA",
+            });
+
+            // Run the request.
+            Console.WriteLine("Executing a list request...");
+            var result = await service.Apis.List().ExecuteAsync();
+
+            // Display the results.
+            if (result.Items != null)
+            {
+                foreach (DirectoryList.ItemsData api in result.Items)
+                {
+                    Console.WriteLine(api.Name + " - " + api.Title);
+                }
+            }
+        }
+    }
+}
---
+++ TravelAssistant/TravesysAI.cs
@@ -0,0 +1,31 @@
+﻿using ApiAiSDK;
+using System;
+
+namespace TravelAssistant
+{
+
+    public class TravesysAI
+    {
+        private ApiAi apiAi;
+
+
+        public TravesysAI()
+        {
+            var config = new AIConfiguration("c17f20ba9bdd40a5ba438cde602efdc0", SupportedLanguage.English);
+            apiAi = new ApiAi(config);
+
+            Console.WriteLine("How may i help you?");
+            var inputText = Console.ReadLine();
+            var response = apiAi.TextRequest(inputText);
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine(response?.Result?.Fulfillment?.Speech);
+
+            foreach (var item in response.Result.Parameters)
+            {
+                Console.WriteLine($"{item.Key} ,  {item.Value}");
+            }
+        }
+
+    }
+}
